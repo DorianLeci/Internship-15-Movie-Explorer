85eebcf8ca011aef3773855293d34dd458c419d5
DorianLeci <dleci00@fesb.hr>
2026-02-26 23:29:39 +0100

    Update "MovieCard" component styling

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,23 +1,31 @@
 import styles from './MovieCard.module.scss';
 import type { Movie } from '../../types/movies';
+import defaultPoster from '../../assets/images/default-poster.png';
 
 interface MovieCardProps {
   movie: Movie;
 }
 
-const TMDB_IMAGE_BASE = 'https://image.tmdb.org/t/p/w300';
+const TMDB_IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
 
 export const MovieCard = ({ movie }: MovieCardProps) => {
   return (
     <div className={styles.card}>
-      <h1>{movie.title}</h1>
+      <div className={styles.favoriteBtn}></div>
+      <h1 className={styles.title}>{movie.title}</h1>
       <img
         className={styles.poster}
-        src={`${TMDB_IMAGE_BASE}${movie.poster_path}`}
+        src={
+          movie.poster_path
+            ? `${TMDB_IMAGE_BASE}${movie.poster_path}`
+            : defaultPoster
+        }
         alt={movie.title}
       />
-      <p>Release year:{movie.release_date}</p>
-      <p>Rating: {movie.vote_average}</p>
+      <div className={styles.info}>
+        <p>Release date: {movie.release_date}</p>
+        <p className={styles.rating}>Rating: {movie.vote_average}</p>
+      </div>
     </div>
   );
 };
