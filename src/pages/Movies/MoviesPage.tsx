ec20795b75de4e181ec1ae3afb2836800555f652
DorianLeci <dleci00@fesb.hr>
2026-03-02 00:15:49 +0100

    New fix

--- src/pages/Movies/MoviesPage.tsx
+++ src/pages/Movies/MoviesPage.tsx
@@ -47,7 +47,6 @@ export const MoviesPage = () => {
         {filters.debouncedQuery !== '' &&
           !showSpinner &&
           !error &&
-          !(activeState === search) &&
           moviesToRender.length === 0 && (
             <EmptyStateCard
               title="No films found for"
